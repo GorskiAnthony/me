16b5004561220d3dfec0275ede1d6f44578b7dd0
Anthony Gorski <anthony.developpeurweb@gmail.com>
2020-12-06 13:48:03 +0100

    add dark mode

---
+++ index.js
@@ -0,0 +1,9 @@
+const TOGGLE = document.querySelector("#toogleA");
+const HTML = document.querySelector("html");
+
+
+TOGGLE.addEventListener("click", () => {
+
+        HTML.classList.toggle("dark");
+    
+})
